8d6619a9e7d0983792ff5d974903b8a473d8ee10
Heru Gunawan <37572051+bnhr@users.noreply.github.com>
2025-03-30 22:39:38 +0700

    chore: update prettier

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,30 +1,21 @@
 module.exports = {
-	// Tailwind CSS Plugin
 	plugins: ['prettier-plugin-tailwindcss'],
-
-	// Core formatting
+	"arrowParens": "always",
+	"bracketSameLine": false,
+	"bracketSpacing": true,
 	"semi": false,
 	"singleQuote": true,
-	"printWidth": 80,
-	"useTabs": true,
-	"tabWidth": 2,
-	"endOfLine": 'lf',
-
-	// Brackets and spacing
-	"arrowParens": "always",
-	"bracketSpacing": true,
-	"bracketSameLine": false,
-	"trailingComma": "all",
-
-	// React/JSX specific
-	"singleAttributePerLine": true,
 	"jsxSingleQuote": false,
-
-	// quotes and HTML
 	"quoteProps": "as-needed",
+	"trailingComma": "all",
+	"singleAttributePerLine": true,
 	"htmlWhitespaceSensitivity": "css",
-
-	// Other
-	"proseWrap": "preserve", // "always" for markdown file
+	"vueIndentScriptAndStyle": false,
+	"proseWrap": "preserve",
+	"insertPragma": false,
+	"printWidth": 80,
+	"requirePragma": false,
+	"tabWidth": 2,
+	"useTabs": true,
 	"embeddedLanguageFormatting": "auto"
 }
